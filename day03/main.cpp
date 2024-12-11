83adedfc85a99761c75c2a47f9bf04a7a36880b5
Nenad Zikic <nenad.zikic@gmail.com>
2024-12-12 00:35:33 +0100

    Day 03 | part 2

--- day03/main.cpp
+++ day03/main.cpp
@@ -21,6 +21,33 @@ int sum_part_1(const std::string& memory)
     return std::ranges::fold_left(muls, 0, std::plus<int>{});
 }
 
+int sum_part_2(const std::string& memory)
+{
+    std::regex conditional_mul{ R"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))" };
+    auto mul_begin = std::sregex_iterator(memory.begin(), memory.end(), conditional_mul);
+    auto mul_end = std::sregex_iterator();
+
+    auto matches = std::ranges::subrange(mul_begin, mul_end);
+    bool enabled = true;
+    auto multiply_conditionally = [&enabled] (auto match) {
+        if (match.str() == "do()") {
+            enabled = true;
+            return 0;
+        }
+
+        if (match.str() == "don't()") {
+            enabled = false;
+            return 0;
+        }
+
+        return enabled
+            ? std::stoi(match[1]) * std::stoi(match[2])
+            : 0;
+    };
+
+    return std::ranges::fold_left(matches | std::views::transform(multiply_conditionally), 0, std::plus<int>{});
+}
+
 int main()
 {
     std::fstream input("input.txt");
@@ -28,12 +55,11 @@ int main()
         puts("Could not open input.txt");
         return 1;
     }
-    
-    //std::string memory = "mul, mul(4), mul(1,2), mulia( 1 ,  3 ),asddmul(!2,4)dsdsmul(10,20)aaaaa_";
+
     std::string memory{std::istreambuf_iterator<char>(input), {}};
 
-
     std::cout << std::format("Sum of multiplications is: {}\n", sum_part_1(memory));
+    std::cout << std::format("Sum of conditional mulitplications is: {}\n", sum_part_2(memory));
 
     return 0;
 }
